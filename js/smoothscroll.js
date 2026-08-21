d8dfd559d26d1b97f02d44aacc176bd1c7455101
Lucile <lucile.guillo27@gmail.com>
2026-08-21 18:06:14 +0200

    fixed

--- js/smoothscroll.js
+++
@@ -1,8 +0,0 @@
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-// Initializes smooth scrolling with GSAP ScrollSmoother.
-ScrollSmoother.create({
-  smooth: 1,
-  effects: true,
-  normalizeScroll: true,
-});
